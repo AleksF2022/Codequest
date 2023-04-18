970ffda3cff3a3ea00e753825610b2ad07250e0a
AleksF2022 <aleksfomin52@gmail.com>
2023-04-18 10:48:01 +0300

    html & css completed
    Signed-off-by: AleksF2022 <aleksfomin52@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const path = require('./config/path.js');
 // Задачи
 const clear = require('./task/clear.js');
 const html = require('./task/html.js');
+const css = require('./task/css.js');
+// const pug = require('./task/pug.js');
 //  Сервер
 const server = () => {
   browserSync.init({
@@ -15,18 +17,23 @@ const server = () => {
 }
 //  Наблюдение
 const watcher = () => {
-  // watch(path.html.watch, html).on('all', browserSync.reload);
-  watch(['src/**/*.html'], html).on('all', browserSync.reload)
+  // watch(['src/**/*.html'], html).on('all', browserSync.reload)
+  // watch(['src/**/*.css'], css).on('all', browserSync.reload)
+  watch(path.css.watch, css).on('all', browserSync.reload)
+  watch(path.html.watch, html).on('all', browserSync.reload);
 }
 // == Callback функция - вывод в консоль
-const consl = cb => {
-  console.log('Вывод в консоль');
-  cb();
-}
+// const consl = cb => {
+//   console.log('Вывод в консоль');
+//   cb();
+// }
 // Экспорт
 exports.html = html;
+exports.css = css;
+// exports.pug = pug;
 exports.watcher = watcher;
-exports.consl = consl;
 exports.clear = clear;
+// exports.consl = consl;
 // Сборка
-exports.dev = series(clear, html, parallel(watcher, server));
+exports.dev = series(clear,
+  parallel(html, css), parallel(watcher, server));
---
+++ task/css.js
@@ -0,0 +1,32 @@
+const { src, dest } = require('gulp');
+// Конфигурация
+const path = require('../config/path.js');
+const app = require('../config/app.js');
+// Плагины
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const csso   = require('gulp-csso');
+const rename = require('gulp-rename');
+const size = require('gulp-size');
+
+// Обработка Css
+const css = () => {
+  return src(path.css.src, {sourcemaps: true})
+    .pipe(plumber({
+      errorHandler: notify.onError(error => ({
+        title: "CSS",
+        message: error.message
+      }))
+    }))
+    .pipe(concat('main.css'))
+    .pipe(autoprefixer({overrideBrowsersList: ['last 10 version']}))
+    .pipe(size({title: "main.css"}))
+    .pipe(dest(path.css.dest, {sourcemaps: true}))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(csso())
+    .pipe(size({title: "main.min.css"}))
+    .pipe(dest(path.css.dest, {sourcemaps: true}))
+}
+module.exports = css;
